f5ec29f627a28d58277ade9ed62f222ee4eba1f8
lumagoni <151628588+slurrrqy@users.noreply.github.com>
2024-03-07 14:22:03 +0100

    selecteer functie die verbonden is met startknop

---
+++ landing.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.container');
+
+    images.forEach(function (image, index) {
+        image.addEventListener('click', function () {
+            images.forEach(function (img) {
+                img.classList.remove('selected');
+            });
+
+            image.classList.add('selected');
+            selectedImage = index + 1; 
+        });
+    });
+});
+
+let selectedImage = null;
+
+function startQuiz() {
+    if (selectedImage !== null) {
+        switch (selectedImage) {
+            case 1:
+                window.location.href = 'quiz1.html';
+                break;
+            case 2:
+                window.location.href = 'quiz2.html';
+                break;
+            case 3:
+                window.location.href = 'quiz3.html';
+                break;
+            default:
+                alert('selecteer een afbeelding');
+        }
+    } else {
+        alert('selecteer een afbeelding');
+    }
+}
